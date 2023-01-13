816d50951c6fcbf48a2e6ca533f9d20616630979
Naomi Phillips <naomi@nep.soy>
2023-01-13 14:50:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# imgswap
+
+Create local copies of media in HTML documents and rewrite documents with the local media
+
+```console
+$ cat in.html 
+<html><body><img src="https://i.redd.it/ueavss99jvba1.jpg"/></body></html>
+$ imgswap.py in.html out.html
+64e4a0ed55dcd38b962d5a8af6e6428be6550aea1908a282c88b3ffdc6898805.jpg: from remote
+Wrote new document to out.html
+1 new media fetched, 0 duplicate media inserted, 0 media inaccessable
+$ cat out.html 
+<html><body><img data-imgswap-src="https://i.redd.it/ueavss99jvba1.jpg" src="imgswap_media/64e4a0ed55dcd38b962d5a8af6e6428be6550aea1908a282c88b3ffdc6898805.jpg"/></body></html>
+```
---
+++ imgswap.py
@@ -0,0 +1,116 @@
+#! /usr/bin/env python3
+
+import argparse
+import hashlib
+import json
+import os
+import requests
+import sys
+import time
+
+import magic
+from bs4 import BeautifulSoup
+
+EXTMAP = {
+    "image/jpeg": "jpg",
+    "image/png": "png",
+    "image/gif": "gif",
+    "image/webp": "webp",
+    "image/avif": "avif",
+}
+
+class Media:
+    def __init__(self, path):
+        self.path = os.path.realpath(path)
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+        elif os.path.isdir(self.path):
+            pass
+        else:
+            sys.exit("Specified media directory is not a directory")
+
+        self.qty_known, self.qty_fetch, self.qty_fail = 0, 0, 0
+
+        known_path = os.path.join(self.path, "known.json")
+        if os.path.exists(known_path):
+            with open(known_path, "rb") as kp:
+                self.known = json.load(kp)
+        else:
+            self.known = {}
+
+    def __repr__(self):
+        return "Media({})".format(repr(self.path))
+
+    """Return the local filename coresponding to a URL, downloading first if applicable"""
+    def process(self, url: str) -> str | None:
+        if url in self.known:
+            self.qty_known += 1
+            print("{}: from known".format(self.known[url]))
+            return self.known[url]
+
+        time.sleep(0.25)
+        try:
+            response = requests.get(url)
+        except:
+            self.qty_fail += 1
+            print("Error downloading {}".format(url))
+            self.known[url] = None
+            return None
+        if not response.ok:
+            self.qty_fail += 1
+            self.known[url] = None
+            return None
+
+        mime = magic.from_buffer(response.content, mime=True)
+        if mime in EXTMAP:
+            extension = EXTMAP[mime]
+        else:
+            self.qty_fail += 1
+            self.known[url] = None
+            return None
+
+        sha = hashlib.sha256(response.content).hexdigest()
+        filename = "{}.{}".format(sha, extension)
+        open(os.path.join(self.path, filename), "wb").write(response.content)
+        self.known[url] = filename
+        print("{}: from remote".format(filename))
+        self.qty_fetch += 1
+        return filename
+
+    """Save known URLs to disk"""
+    def save_known(self):
+        known_file = os.path.join(self.path, "known.json")
+        dump = json.dumps(self.known)
+        open(known_file, "w").write(dump)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("infile", help="File to process")
+parser.add_argument("outfile", help="Destination for processed file")
+parser.add_argument("--media", required=False, help="Media directory")
+args = parser.parse_args()
+
+# Add default media directory based on `outfile`
+if args.media is None:
+    parent = os.path.dirname(os.path.realpath(args.outfile))
+    args.media = os.path.join(parent, "imgswap_media")
+
+media = Media(args.media)
+
+# Load `infile`
+with open(args.infile, "rb") as infile:
+    soup = BeautifulSoup(infile, features="lxml")
+
+# Iterate over elements, processing each <img>
+for elem in soup.find_all("img"):
+    local = media.process(elem["src"])
+    if local is None:
+        continue
+    elem["data-imgswap-src"] = elem["src"]
+    elem["src"] = os.path.join("imgswap_media", local)
+
+media.save_known()
+
+final_html = str(soup)
+open(args.outfile, "w").write(final_html)
+print("Wrote new document to {}".format(args.outfile))
+print("{} new media fetched, {} duplicate media inserted, {} media inaccessable".format(media.qty_fetch, media.qty_known, media.qty_fail))
